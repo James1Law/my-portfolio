0eb9bad38922e7b8780c39d4fdbc7dc8b4ddffe8
James1Law <james.law@90poe.io>
2025-03-13 09:15:18 +0000

    aha!

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -4,7 +4,7 @@ import { SiAsana, SiMiro } from 'react-icons/si';
 
 const AhaIcon = () => (
   <svg viewBox="0 0 24 24" width="1em" height="1em" fill="#2673E8">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" />
+    <path d="M4 2C2.9 2 2 2.9 2 4v16c0 1.1 0.9 2 2 2h16c1.1 0 2-0.9 2-2V4c0-1.1-0.9-2-2-2H4zm8 4c0.55 0 1 0.45 1 1v6c0 0.55-0.45 1-1 1s-1-0.45-1-1V7c0-0.55 0.45-1 1-1zm0 10c0.55 0 1 0.45 1 1s-0.45 1-1 1s-1-0.45-1-1s0.45-1 1-1z" />
   </svg>
 );
 
